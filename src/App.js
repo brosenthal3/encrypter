d8efce422ff87b96006a550b70aee32e4dc1e8b2
Ben Rosenthal <benben.rosenthal@gmail.com>
2019-08-11 21:24:31 +0200

    small updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as HashRouter, Route} from 'react-router-dom';
 
 import SelectPage from './pages/select-page/select-page.page';
 import {EncryptPage} from './pages/encrypt-page/encrypt-page.page';
@@ -10,14 +10,14 @@ import {Credits} from './components/credits/credits.component';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter basename='/'>
         <div className="page">
           <Credits />
             <Route path="/" exact component={SelectPage} />
             <Route path="/decrypt" exact component={DecryptPage} />
             <Route path="/encrypt" exact component={EncryptPage} />
         </div>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
